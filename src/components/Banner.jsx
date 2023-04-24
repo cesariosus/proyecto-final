f38df289dc322f78289799d377fb7043d31ce6fa
LandyYaoo <llpayaoo@hotmail.com>
2023-04-23 21:20:44 -0500

    contacto banner + form, falta estilos boton

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import stylesBanner from '../css/Banner.module.css'
+
+function Banner(props) {
+  const {imagen, titulo } = props;
+  return (
+    <section>
+      <img className={stylesBanner.imgBanner} src={imagen} alt="" />
+      <h1 className={stylesBanner.title}>{titulo}</h1>
+    </section>
+  )
+}
+
+export default Banner
