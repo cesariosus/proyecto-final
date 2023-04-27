8f6eac62c18cd9917b4f348b81eaa30246e65bcf
“01FERNANDAGALINDO” <fernanda3108182@gmail.com>
2023-04-27 15:49:48 -0500

    login

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import estilos from "../css/login.module.css"
 
 function LogIn() {
   return (
-    <div>LogIn</div>
+    <div className={estilos.loginbox} >
+    <img className={estilos.avatar} src="img/turismo-e1552499811477.jpg" alt="logo de fazt" />
+    <h1>Login Here</h1>
+    <form>
+        {/* USERNAME */}
+        <label htmlFor="username">USERNAME</label>
+        <input type="text" placeholder="ENTER USERNAME" />
+        {/* PASSWORD */}
+    <label htmlFor="password">PASSWORD</label>
+    <input type="password" placeholder="Enter password" />
+
+    <input type="submit" value="login" />
+
+    <a href="#">lost your password?</a><br />
+    <a href="#">Don't have an account?</a>
+</form>
+</div>
   )
 }
 
