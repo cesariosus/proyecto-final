6df4c87e6968bd3d74cbb9125551e9fb9f6ea669
LandyYaoo <llpayaoo@hotmail.com>
2023-05-01 10:57:22 -0500

    agregando data

---
+++ src/data/posts.js
@@ -0,0 +1,22 @@
+const PostsData = [
+  {
+    id: 1,
+    title:
+      "LOS ESTRENOS MÁS DESTACADOS DE NETFLIX, DISNEY+ Y HBO MAX (DEL 21 AL 28 DE SEPTIEMBRE)",
+    content:
+      "Semana a semana Netflix, Disney+ y Hbo Max renuevan su catálogo de contenidos con nuevos títulos para las diferentes audiencias. Repasamos cuáles son los estrenos más destacados en materia de streaming del 21 al 28 de septiembre.",
+    tags: ["steaming", "netflix", "disney+", "hbo max"],
+    imageUrl: "https://spinofftv.ar/wp-content/uploads/2022/09/Andor.jpg",
+  },
+  {
+    id: 2,
+    title: "NOVEDADES DE LAS PRODUCCIONES LATINAS DE NETFLIX",
+    content:
+      "Este año llegará una gran variedad de series y películas latinoamericanas a Netflix, que a través de un evento para la prensa, reveló detalles de los títulos en desarrollo. Entre las producciones argentinas se encuentran la segunda temporada de El reino que ya comenzó con las grabaciones y El amor después del amor, la bioserie de Fito Páez.",
+    tags: ["steaming", "netflix", "argentina"],
+    imageUrl:
+      "https://spinofftv.ar/wp-content/uploads/2022/04/COLLAGE_EDITABLE_V3-scaled.jpg",
+  },
+];
+
+export default PostsData;
---
+++ src/data/users.js
@@ -0,0 +1,14 @@
+const usersData = [];
+usersData.push({
+  id: 1,
+  username: "administrador",
+  password: "admin",
+  roll: "admin",
+});
+usersData.push({
+  id: 2,
+  username: "Leidy",
+  password: "12345",
+  roll: "usuario",
+});
+export default usersData;
